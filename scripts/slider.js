392e305380aa17e6e9626cc48ab4a649b24f18f5
TheFranck <f-sarin@sfr.fr>
2017-06-28 12:08:52 +0000

    Creation of slider images are hidden, except for the first one

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
-  initializeSlider();
 
+  var sliderHtml = " "; // variable qui va stocker le html de notre slider
+  var sliderData;
+  var height = "500";
+
+
+  initializeSlider();
+  createSlider();
+  render();
 
 
   function initializeSlider()
   {
-    var sliderData = $('#slider-container img');
+    sliderData = $('#slider-container img');
     // Load html hook
     if (sliderData.length == 0) {
       // if the hook is broken, throw error to stop script
@@ -15,5 +22,31 @@ $(document).ready(function() {
   }
 
 
+  function render()
+  {
+    $('body').append(sliderHtml);
+  }
+
+  function createSlider()
+  {
+    sliderHtml += "<div id='slider-container' style='display:flex; flex-direction:row; overflow:hidden;'>";
+
+      for (var i = 0; i < sliderData.length; i++) {
+        var display;
+        if (i == 0) {
+          display = "display:flex; justify-content:center;";
+        } else {
+          display = "display:none;";
+        }
+
+        sliderHtml += "<div style='width:100%;height:500px;" + display + "'>";
+        sliderHtml += "<img src='" + $(sliderData[i])[0].src + "' style='height:" + height + "px;width:auto'>";
+        sliderHtml += "</div>";
+      }
+
+    sliderHtml += "</div>";
+    sliderHtml += "<button data-action='left'><</button>";
+    sliderHtml += "<button data-action='right'>></button>";
+  }
 
 });
